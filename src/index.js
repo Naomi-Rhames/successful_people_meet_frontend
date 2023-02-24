b421a44f032e210547ca6bcdeed0563950ac0c20
github username <sharnellgrant@gmail.com>
2023-02-23 19:03:16 -0500

    connected redux and got the App.js page done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import {  compose } from 'redux';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+const store = configureStore (composeEnhancers);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
